87d2a850b3e99d09a43595da696e2289bada0534
Ishmael Sunday <okayishmael@gmail.com>
2025-02-08 10:30:03 -0500

    updated html, css , js

--- script.js
+++ script.js
@@ -1,13 +1,4 @@
-
-
-
 function showShareLinks() {
-    var shareActive = document.getElementById("share-active-block");
-    if (shareActive.style.display === "none") {
-      shareActive.style.display = "block";
-    } else {
-      shareActive.style.display = "none";
-    }
-  } 
-
-  
+  var shareActive = document.getElementById("share-active-block");
+  shareActive.classList.toggle("hide");
+}
